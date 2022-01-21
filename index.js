0a35c74cea9439b36e280edf3174562f588f5492
Suman196pokhrel <spokhrel196@gmail.com>
2022-01-21 12:12:36 +0530

    first proto finished

--- index.js
+++ index.js
@@ -3,29 +3,36 @@ const bg = document.querySelector('.bg')
 var slider = document.getElementById("myRange");
 
 let load = 0
-let int = setInterval(blurring, 20)
+    // let int = setInterval(blurring, 20)
 
-function blurring() {
-    load++
+// function blurring() {
+//     load++
 
-    if (load > 99) {
-        clearInterval(int)
-    }
+//     if (load > 99) {
+//         clearInterval(int)
+//     }
 
 
+//     loadText.innerText = `${load}%`
+//     loadText.style.opacity = scale(load, 0, 100, 1, 0)
+//         // we are doing so beacuse we wan't to go from opaque to transparent    its value is going from 0 to 1
+
+
+//     bg.style.filter = `blur(${scale(load,0,100,30,0)}px)`
+//         // its value is going from 30 to 0 in same amount
+// }
+const scale = (number, inMin, inMax, outMin, outMax) => {
+    return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+}
+
+slider.addEventListener("change", (e) => {
+    // alert(e.target.value)
+    load = e.target.value
     loadText.innerText = `${load}%`
     loadText.style.opacity = scale(load, 0, 100, 1, 0)
-        // we are doing so beacuse we wan't to go from opaque to transparent    its value is going from 0 to 1
-
 
     bg.style.filter = `blur(${scale(load,0,100,30,0)}px)`
-        // its value is going from 30 to 0 in same amount
-}
-const scale = (number, inMin, inMax, outMin, outMax) => {
-        return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
-    }
-    // this function is copied from stack overflow
 
-slider.addEventListener(onchange, (e) => {
-    alert("hi")
+
+
 })
